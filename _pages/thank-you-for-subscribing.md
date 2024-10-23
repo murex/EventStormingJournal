e0ce43b1fedd8e607a4f9e9de84e97c104b40b08
Philippe Bourgau <philippe.bourgau@gmail.com>
2024-10-23 16:25:26 +0200

    Download book pre-release on subscription

--- _pages/thank-you-for-subscribing.md
+++ _pages/thank-you-for-subscribing.md
@@ -7,6 +7,6 @@ author_profile: true
 ---
 We are very happy to count you as part of our regular readers! Expect to read our new content from your emailbox pretty soon!
 
-Meanwhile, download this [recipe for a do-it-yourself workshop]({{site.url}}/assets/2022-09-05-engage-the-participants-during-your-next-event-storming-thanks-to-the-delegated-missions/DIY workshop material ES & DM.zip) that you can run with your team to get started with Event Storming and Delegated Missions in 2 hours.
+Meanwhile, download the current release of our [1-hour Event Storming Book]({{site.url}}/the-1-hour-event-storming-book/The-1-hour-Event-Storming-book.epub) and get started with Event Storming today!
 
-[![Illustration of a book titled "PDF" with a big red downward arrow that represents downloading.]({{site.url}}/assets/images/pdf-download.png)]({{site.url}}/assets/2022-09-05-engage-the-participants-during-your-next-event-storming-thanks-to-the-delegated-missions/DIY workshop material ES & DM.zip)
+[![Illustration of a book titled "PDF" with a big red downward arrow that represents downloading.]({{site.url}}/assets/images/pdf-download.png)]({{site.url}}/the-1-hour-event-storming-book/The-1-hour-Event-Storming-book.epub)
--- the-1-hour-event-storming-book/_publish_new_version.sh
+++ the-1-hour-event-storming-book/_publish_new_version.sh
@@ -4,29 +4,43 @@ VERSION=$1
 VERSION_LATEST="latest (unreleased)"
 VERSION_AND_DATE="$VERSION (`date`)"
 
-echo "Setting version '$VERSION' 'index.Rmd'..."
+echo "===> Setting version '$VERSION' 'index.Rmd'..."
 sed "s/$VERSION_LATEST/$VERSION_AND_DATE/g" -i index.Rmd
+echo "==="
+echo
 
-echo "Generating the epub..."
+echo "===> Generating the epub..."
 ./_build.sh
+echo "==="
+echo
 
-echo "Copying it to the repo..."
+echo "===> Copying it to the repo..."
 cp _book/The-1-hour-Event-Storming-book.epub ./The-1-hour-Event-Storming-book.epub
+echo "==="
+echo
 
-echo "Creating a commit..."
+echo "===> Creating a commit..."
 git add --force index.Rmd The-1-hour-Event-Storming-book.epub
 git commit --message "New book version: $VERSION"
+echo "==="
+echo
 
-echo "Adding a tag..."
+echo "===> Adding a tag..."
 git tag $VERSION
+echo "==="
+echo
 
-echo "Setting version '$VERSION_LATEST' 'index.Rmd'..."
+echo "===> Setting version '$VERSION_LATEST' 'index.Rmd'..."
 sed "/$VERSION_AND_DATE/i **$VERSION_LATEST**\n\n- \n\n---" -i index.Rmd
 git add index.Rmd
 git commit --message "Add $VERSION_LATEST section in version information"
+echo "==="
+echo
 
-echo "Pushing..."
+echo "===> Pushing..."
 git push --follow-tags
+echo "==="
+echo
 
-echo "You: Send a ConvertKit broadcast to all subscribers with the summary of the changes since last version, and link to download the latest version. (Press a key when done)"
+echo "===> YOU: Send a kit.com broadcast to all subscribers with the summary of the changes since last version, and link to download the latest version. Duplicate the "[Event Storming Journal] \<VERSION\> \"The 1-hour Event Storming Book\" https://app.kit.com/campaigns/16994614/draft broadcast in kit.com" (Press a key when done)"
 read
